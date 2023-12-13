7a65cdf31efa418349ba3155feda10496c3bf14c
igorfmartins <igor.dev.martins@outlook.com>
2023-12-13 11:28:44 -0300

    add_new_RecipeTypes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root "home#index"
   resources :recipe_types, only: %i[new create index show]
   resources :recipes, only: %i[new create edit update show]
